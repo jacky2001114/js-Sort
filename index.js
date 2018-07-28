62f23fab3d1e1c285d42354dbbbb1643bbc73f29
bot_3310 <jacky2001114@gmail.com>
2018-07-28 13:04:00 +0800

    beatify

--- index.js
+++ index.js
@@ -1,100 +1,93 @@
-function bbsort(array,objectProp,method){
+function bbsort(array, objectProp, method) {
 
-    
-    if(!method&&objectProp===-1){
-    
-       method=objectProp;
-       objectProp=undefined;
+    if (!method && objectProp === -1) {
+
+        method = objectProp;
+        objectProp = undefined;
     }
-    
 
     //check param is array
     var isArray = Array.isArray(array)
-    if(!isArray){
+    if (!isArray) {
         console.error('BBSort Error : You are not sorting a array')
         return array
-       
-    }else{
-       if(objectProp===undefined){
-            if(method===undefined){
 
-                for(var i=0;i<array.length;i++){
+    } else {
+        if (objectProp === undefined) {
+            if (method === undefined) {
 
-                        for(var j=0;j<array.length;j++){
+                for (var i = 0; i < array.length; i++) {
 
-                            if(array[j]>array[j+1]){
+                    for (var j = 0; j < array.length; j++) {
 
-                                var temp = array[j];
-                                array[j] = array[j+1];
-                                array[j+1] = temp;
+                        if (array[j] > array[j + 1]) {
 
-                            }
+                            var temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
 
                         }
 
+                    }
+
                 }
-            }else if(method===-1){
-                console.log('fgrfg')
-               
-                    
-                for(var i=0;i<array.length;i++){
-        
-                        for(var j=0;j<array.length-1;j++){
-        
-                            if(array[j+1]>array[j]){
-        
-                                var temp = array[j];
-                                array[j] = array[j+1];
-                                array[j+1] = temp;
-        
-                            }
-        
+            } else if (method === -1) {
+
+                for (var i = 0; i < array.length; i++) {
+
+                    for (var j = 0; j < array.length - 1; j++) {
+
+                        if (array[j + 1] > array[j]) {
+
+                            var temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+
                         }
-        
+
+                    }
+
                 }
-                    
+
+            }
+            //3 params
+        } else {
+
+            if (method === undefined) {
+                for (var i = 0; i < array.length; i++) {
+
+                    for (var j = 0; j < array.length - 1; j++) {
+
+                        if (array[j][objectProp] > array[j + 1][objectProp]) {
+
+                            var temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+
+                        }
+
+                    }
+
                 }
-        //3 params
-        }else{
-           
-            if(method===undefined){
-                for(var i=0;i<array.length;i++){
-                    
-                     for(var j=0;j<array.length-1;j++){
-                       
-                          
-                         if(array[j][objectProp]>array[j+1][objectProp]){
-                          
-                             var temp = array[j];
-                             array[j] = array[j+1];
-                             array[j+1] = temp;
-     
-                         }
-                      
-                     }
-     
-                 }
-            }else if(method===-1){
-                for(var i=0;i<array.length;i++){
-                    
-                     for(var j=0;j<array.length-1;j++){
-                       
-                          
-                         if(array[j+1][objectProp]>array[j][objectProp]){
-                          
-                             var temp = array[j];
-                             array[j] = array[j+1];
-                             array[j+1] = temp;
-     
-                         }
-                      
-                     }
-     
-                 }
+            } else if (method === -1) {
+                for (var i = 0; i < array.length; i++) {
+
+                    for (var j = 0; j < array.length - 1; j++) {
+
+                        if (array[j + 1][objectProp] > array[j][objectProp]) {
+
+                            var temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+
+                        }
+
+                    }
+
+                }
             }
 
         }
         return array;
     }
 }
-
